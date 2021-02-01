c0baba7b9597c9582b84564e72d099a2d8a7cb1c
Jonathan Villalobos Mora <hackertristania@gmail.com>
2021-02-01 20:25:29 +0000

    React Exercise

--- exercises/01.1-hello-world/index.js
+++ exercises/01.1-hello-world/index.js
@@ -2,10 +2,13 @@ import React from "react"; //Main React.js library
 
 import ReactDOM from "react-dom"; //we use ReactDOM to render into the DOM
 
+let age = 12;
+let name = "John";
+
 // WHAT: This variable returns contains the html to render
 let output = (
 	<span>
-		James is 12 years old
+		{name} is{age} years old
 	</span>
 );
 
